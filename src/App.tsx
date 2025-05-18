698ecd9c4c4674d6b58c834d16c797b38c507d70
John Royal <34844819+john-royal@users.noreply.github.com>
2025-05-18 11:17:48 -0400

    ddd

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Use Functional please</p>
+      <p className="read-the-docs">Use Functional please!</p>
     </>
   );
 }
